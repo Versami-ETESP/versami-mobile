b70049fa5984df3f0dfc944a1aa96a433464d448
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-03-15 12:37:44 -0300

    Aparencia da tela de boas vindas

--- prjVersami/app/src/main/java/com/example/prjversami/views/telaBemvindo.java
+++ prjVersami/app/src/main/java/com/example/prjversami/views/telaBemvindo.java
@@ -4,20 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.prjversami.R;
 
 public class telaBemvindo extends AppCompatActivity {
 
+    Button btnNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_bemvindo);
+        btnNext = findViewById(R.id.btnIniciar);
         getSupportActionBar().hide();
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(telaBemvindo.this, login.class);
+                startActivity(intent);
+            }
+        });
+
     }
-    public void proxTela(View view) {
-        // Cria uma nova intenção para abrir a outra ACtivity
-        Intent intent = new Intent(telaBemvindo.this, login.class);
-        startActivity(intent);
-    }
+
 }
