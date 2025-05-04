3100d13713cdbc0a5e784eda7ff1b5296376168b
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-05-04 15:54:58 -0300

    tela de editar perfil design

---
+++ prjVersami/app/src/main/java/com/example/prjversami/EditarPerfil.java
@@ -0,0 +1,14 @@
+package com.example.prjversami;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+        getSupportActionBar().setTitle("Editar Perfil");
+    }
+}
